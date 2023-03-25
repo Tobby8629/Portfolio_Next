387900f76faaf5f03d10c92d66f8d3e7e8b3b2f1
tobby8629 <sarhmullah15@gmail.com>
2023-03-25 09:33:10 -0700

    modify project section

--- pages/projects/others.jsx
+++ pages/projects/others.jsx
@@ -9,20 +9,43 @@ const Utility = () => {
     <>
       <ProNav />
       <div className={styles.main}>
-        {others.map((ut) => (
-          <div className={styles.each} key={ut.id}>
-            <Link href={ut.project}>
-              <div className={styles.img}>
-                <img src={ut.image} alt="project view" />
-              </div>
-              <div className={styles.text}>
-                <h3>{ut.title}</h3>
-              </div>
-            </Link>
+    {others.map((pro) => (
+      <div className={styles.each} key={pro.id}>
+      
+          <div className={styles.img}>
+            <img src={pro.image} alt="project view" />
           </div>
+         
+          <h3>{pro.title}</h3>
 
-        ))}
+          <div className={styles.shadow}>
+
+            <div className={styles.top}>
+              <h3>{pro.title}</h3>
+              <h4>{pro.group}</h4>
+            </div>
+
+            <div className={styles.mid}>
+              <p>{pro.desc}</p>
+            </div>
+
+            <ul className={styles.stacks}>
+              {pro.stacks.map((stack)=>(
+                <li key={Math.floor(Math.random()*200)}>{stack}</li>
+              ))}
+            </ul>
+
+            <div className={styles.btns}>
+             <Link href={pro.project}>See live</Link>
+             <Link href={pro.source}> see demo</Link>
+            </div>
+
+          </div>
+          
       </div>
+
+    ))}
+  </div>
     </>
   );
 };
--- public/projects.js
+++ public/projects.js
@@ -1,22 +1,34 @@
 const project = [
   {
-    id: 1, project: 'https://metric-app-4bso.onrender.com/', image: '/metricApp1.png', group: 'metric', title: 'Metric App',
+    id: 1, project: 'https://metric-app-4bso.onrender.com/', image: '/metricApp1.png', group: 'metric', title: 'Metric App', source: 'https://github.com/Tobby8629/react-capstone.git',
+    desc: "React Metric WebApp is a webApp that contains Apple product annual revenue, displaying them in segment of year, and into a smaller segment of each country revenue.",
+    stacks: ['React', 'CSS', 'Redux', 'Render'],
   },
   {
-    id: 2, project: 'https://tobby-math-magician.onrender.com/arithemetic', image: '/metricApp2.png', group: 'metric', title: 'Math App',
+    id: 2, project: 'https://tobby-math-magician.onrender.com/arithemetic', image: '/metricApp2.png', group: 'metric', title: 'Math App', source: 'https://github.com/Tobby8629/math_magician.git',
+    desc: 'math magician is a simple mathmatics website that says alot amd promote the mathematics community, it consist of feature such as calculator that run simple arithemetics',
+    stacks: ['React', 'CSS', 'Redux', 'Render']
   },
   {
-    id: 3, project: 'https://tobby8629.github.io/Tobby-portfolio/', image: '/portfolio1.png', group: 'portfolio', title: 'Tobby Porfolio',
+    id: 3, project: 'https://tobby8629.github.io/Tobby-portfolio/', image: '/portfolio1.png', group: 'portfolio', title: 'Tobby Porfolio', source: 'https://github.com/Tobby8629/Tobby-portfolio.git',
+    desc: 'A portfolio project, which houses all projects, resumes, and social links to reach tobby, the project has a project section, an about myself section, contact me.',
+    stacks: ['Html', 'CSS', 'JavaScript'],
   },
   {
-    id: 4, project: 'https://tobby8629.github.io/Capstone-project/index.html', image: '/Nts.png', group: 'others', title: 'Nts Summit',
+    id: 4, project: 'https://tobby8629.github.io/Capstone-project/index.html', image: '/Nts.png', group: 'others', title: 'Nts Summit', source: 'https://github.com/Tobby8629/Capstone-project.git',
+    desc: 'A National summit website, that describes all activities involved and expected from the summit.',
+    stacks: ['Html', 'CSS', 'JavaScript'],
   },
   {
-    id: 5, project: 'https://tobby8629.github.io/Todo_list/dist/', image: '/todolist.png', group: 'utility', title: 'Todo List',
+    id: 5, project: 'https://tobby8629.github.io/Todo_list/dist/', image: '/todolist.png', group: 'utility', title: 'Todo List', source: 'https://github.com/Tobby8629/Todo_list.git',
+    desc: "Todo_list is a project built with webpack with alot features such as adding and removing from the todo-list, dragable event, clear completed tasks",
+    stacks: ['Html', 'CSS', 'Webpack', 'JavaScript']
   },
   {
-    id: 6, project: 'https://space-x-vv8h.onrender.com/', image: '/spaceApp.png', group: 'others', title: 'Space X',
-  },
+    id: 6, project: 'https://space-x-vv8h.onrender.com/', image: '/spaceApp.png', group: 'others', title: 'Space X', source: 'https://github.com/Mekuseo/Space-Travellers-Hub.git',
+    desc: 'Space Traveller Hub App is a rocket website with features of making reservations, cancel reservation and a profile page that upfates the state of reservation of each rocket.',
+    stacks: ['React', 'CSS', 'Redux', 'Render']
+  }, 
 ];
 
 export default project;
