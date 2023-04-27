df42aad6bc9ff4c0ca101bbf65f13a310f9a5be0
tobby8629 <sarhmullah15@gmail.com>
2023-04-27 05:43:45 -0700

    fix navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const [menu, setmenu] = useState(true)
+
+  useEffect(()=>{
+    const dav = document.getElementById("nav")
+    const run = window.addEventListener("scroll", ()=>{
+      if (scrollY>10) {
+        dav.style.backgroundColor = "white"
+      }
+      else {
+        dav.style.backgroundColor = "transparent"
+      }
+    })
+  })
+   
+ 
 return(
-   <nav>
+   <nav id ='nav'>
     <ul className= {menu ? 'hide' : 'links'} onClick={()=>setmenu(true)}>
       <li><Link href="/">Home</Link></li> 
       <li><Link href="/About">About</Link></li>
@@ -17,7 +31,7 @@ return(
     </div>
   </nav>
 )
- 
+
 };
 
 export default Navbar;
